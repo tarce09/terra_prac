158e72d9d41d3af5e3b874ad41f43a5ca00a3d93
T ARYAN RAJU <69902705+tarce09@users.noreply.github.com>
2023-02-08 13:18:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,34 +7,4 @@ resource "aws_instance" "multiple_ins" {
     #Name = var.names[count.index]
     Name = each.value
   }
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  depends_on = [
-    "aws_security_group.allow_tls"
-  ]
-}
-
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
-}
+  
